4f721576a9fc8d3f6ed6ddbee5e51af4791a1e5d
yunxilu <lu_sissi@126.com>
2019-06-22 10:15:59 +0800

    此demo除了字符串一无所有

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody m_Rigidbody;
+    private Vector3 m_Movement;
+    public float speed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Rigidbody = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A)) {
+            this.transform.position += Vector3.left * speed;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            this.transform.position += Vector3.right * speed;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.position += Vector3.forward * speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.position += Vector3.back * speed;
+        }
+    }
+}
